ea1dcaea959c376f1a28bbae3c0a6666cfe8d685
xNovax <nova1104@live.com>
2014-02-21 21:23:54 -0500

    renamed form

--- Multi-Tool V2/program - Home.Designer.cs
+++ Multi-Tool V2/program - Home.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // programHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 316);
+            this.Name = "programHome";
             this.Text = "programHome";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
